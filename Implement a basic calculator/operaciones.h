f4ded4d12a420df9b31626cab4b3d61882d426c2
buitraaa <buitradaniel.f@gmail.com>
2025-11-09 23:41:36 -0500

    Implement a basic calculator
    Implement a basic calculator

---
+++ Implement a basic calculator/operaciones.h
@@ -0,0 +1,9 @@
+#ifndef operaciones_h
+#define operaciones_h
+
+float sumar(float a, float b);
+float restar(float a, float b);
+float multiplicar(float a, float b);
+float dividir(float a, float b);
+
+#endif
