1e70fe21ca68502bb95b7cdf945f82b8183866ab
buitraaa <buitradaniel.f@gmail.com>
2025-11-10 23:00:54 -0500

    Create bmi.h

---
+++ Application and Systems/Calorie and BMI calculator/bmi.h
@@ -0,0 +1,11 @@
+#ifndef BMI_H
+#define BMI_H
+
+extern float peso, altura,bmi;
+extern int edad, sexo, actividadFisica;
+
+int datosUsuario();
+float calcularBMI();
+float calcularTMB();    
+
+#endif
