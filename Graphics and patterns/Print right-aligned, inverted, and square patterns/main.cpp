c56ca47456e824c77c63974cd13513572502f3d8
buitraaa <buitradaniel.f@gmail.com>
2025-11-12 17:55:46 -0500

    Create main.cpp

---
+++ Graphics and patterns/Print right-aligned, inverted, and square patterns/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "patrones.h"
+using namespace std;
+
+int main() {
+    imprimiralineado();
+    cout << endl;
+    imprimirinvertido();
+    cout << endl;
+    imprimercuadrado();
+    return 0;
+}
