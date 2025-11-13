cb37cfa83dca02114983e0910772b7b9a015ac83
buitraaa <buitradaniel.f@gmail.com>
2025-11-12 21:05:22 -0500

    Create main.cpp

---
+++ Graphics and patterns/Print zig zag/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "zigzag.h"
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Ingrese el tamaño N: ";
+    cin >> n;
+    imprimirzigzag(n);
+    cin.get();
+    cin.get();
+    return 0;
+}
