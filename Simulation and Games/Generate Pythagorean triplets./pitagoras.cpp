be181199c3fa1a6a218cfd887aea1fbb6abd2e76
buitraaa <buitradaniel.f@gmail.com>
2025-11-13 20:53:01 -0500

    Add files via upload

---
+++ Simulation and Games/Generate Pythagorean triplets./pitagoras.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void generarpitagoras(int n) {
+    for (int a = 1; a <= n; a++) {
+        for (int b = a; b <= n; b++) {
+            for (int c = b; c <= n; c++) {
+                if (a*a + b*b == c*c) {
+                    cout << a << " " << b << " " << c << endl;
+                }
+            }
+        }
+    }
+}
---
+++ Simulation and Games/Generate Pythagorean triplets./pitagoras.h
@@ -0,0 +1,6 @@
+#ifndef PITAGORAS_H
+#define PITAGORAS_H
+
+void generarpitagoras(int n);
+
+#endif
